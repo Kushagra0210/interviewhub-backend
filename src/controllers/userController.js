6447465b2863c8c0d09ce0cd15be7a63d28881bb
Kushagra0210 <kushagrasaxena0210@gmail.com>
2026-06-23 05:52:16 +0530

    addded params and query apis

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,8 +4,11 @@ const getUsers = (req, res) => {
         users: ["Kushagra", "Aman"]
     });
 };
+
 const createUser = (req, res) => {
+
     const { name, email } = req.body;
+
     res.json({
         success: true,
         message: "user created successfully",
@@ -15,7 +18,26 @@ const createUser = (req, res) => {
         }
     });
 };
+
+const getSingleUser = (req, res) => {
+
+    const userId = req.params.id;
+
+    res.json({
+        success: true,
+        message: `Fetched user ${userId}`,
+    });
+};
+const searchUsers = (req, res) => {
+    const page = req.query.page;
+    res.json({
+        success: true,
+        currentpage: page
+    })
+}
 module.exports = {
     getUsers,
     createUser,
+    getSingleUser,
+    searchUsers,
 };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { getUsers, createUser } = require("../controllers/userController");
+const { getUsers, createUser, getsingleUser, getSingleUser, searchUsers } = require("../controllers/userController");
 router.get("/", getUsers);
 router.post("/", createUser);
+router.get("/search/query", searchUsers);
+router.get("/:id", getSingleUser);
 module.exports = router;
